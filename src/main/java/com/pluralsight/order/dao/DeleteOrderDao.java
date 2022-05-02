c4b1654c8b7690957705601bbdffff03a22c8240
Rrok Luli <rrokluli@gmail.com>
2022-05-03 00:07:04 +0200

    Update DeleteOrderDAO

--- src/main/java/com/pluralsight/order/dao/DeleteOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/DeleteOrderDao.java
@@ -34,7 +34,7 @@ public class DeleteOrderDao {
         try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderIds())
         ) {
-
+            numberResults = ps.executeUpdate();
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
         }
