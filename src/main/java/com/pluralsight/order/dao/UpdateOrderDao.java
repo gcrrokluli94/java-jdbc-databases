76fee4a5a92d52371f678438a3a258c76eaeb06d
Rrok Luli <rrokluli@gmail.com>
2022-05-13 23:38:11 +0200

    Insert an Order

--- src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
@@ -6,7 +6,6 @@ import com.pluralsight.order.util.ExceptionHandler;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
